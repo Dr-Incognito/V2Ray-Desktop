6c7ead9f06cfa1847845d4e7be8d70318c6481cc
Dr. Incognito <17879520+Dr-Incognito@users.noreply.github.com>
2020-01-19 10:44:42 +0800

    Use clang-format to format the C++ code.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
 
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/ui/main.qml"));
+  QObject::connect(
+    &engine, &QQmlApplicationEngine::objectCreated, &app,
+    [url](QObject *obj, const QUrl &objUrl) {
+      if (!obj && url == objUrl) QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  engine.load(url);
+  return app.exec();
 }
--- main.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.12
-import QtQuick.Window 2.12
-
-Window {
-    visible: true
-    width: 640
-    height: 480
-    title: qsTr("Hello World")
-}
