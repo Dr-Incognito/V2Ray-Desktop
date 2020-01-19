d17ba385fa603bb709714436866a6433cb6d727c
Dr. Incognito <17879520+Dr-Incognito@users.noreply.github.com>
2020-01-19 10:33:51 +0800

    Initially set up the project with QML.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ main.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+
+Window {
+    visible: true
+    width: 640
+    height: 480
+    title: qsTr("Hello World")
+}
