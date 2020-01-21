ed86131596f96a910b044abc030efffa7c1c2b78
Dr. Incognito <17879520+Dr-Incognito@users.noreply.github.com>
2020-01-21 22:57:52 +0800

    Complete add new V2Ray and Shadowsocks server.

--- src/appproxy.h
+++ src/appproxy.h
@@ -1,6 +1,7 @@
 #ifndef APPPROXY_H
 #define APPPROXY_H
 
+#include <QJsonObject>
 #include <QObject>
 #include <QString>
 
@@ -12,7 +13,6 @@ class AppProxy: public QObject {
 public:
     AppProxy();
 
-    QString getV2RayCoreStatusTmp();
 signals:
     void appVersionReady(QString appVersion);
     void v2RayCoreVersionReady(QString v2RayCoreVersion);
@@ -20,9 +20,12 @@ signals:
     void v2RayCoreStatusReady(QString v2RayCoreStatus);
     void v2RayRunningStatusChanging(bool isChanged);
     void appConfigReady(QString appConfig);
+    void appConfigError(QString errorMessage);
     void appConfigChanged();
     void logsReady(QString logs);
     void serversReady(QString servers);
+    void addServerError(QString errorMessage);
+    void serversChanged();
 
 public slots:
     QString getAppVersion();
@@ -31,14 +34,24 @@ public slots:
     QString getV2RayCoreStatus();
     bool setV2RayCoreRunning(bool expectedRunning);
     QJsonObject getAppConfig();
-    void saveAppConfig(QString appConfig);
+    void saveAppConfig(QString configString);
     QString getLogs();
     void clearLogs();
     QJsonArray getServers();
+    void addV2RayServer(QString configString);
+    void addShadowsocksServer(QString configString);
+    void addSubscriptionUrl(QString subsriptionUrl);
+    void addServerConfigFile(QString configFilePath);
+    void editServer(QString serverName, QString configString);
+    void removeServer(QString serverName);
 
 private:
     V2RayCore& v2ray;
     Configurator configurator;
+    QJsonObject getPrettyV2RayConfig(const QJsonObject &serverConfig);
+    QJsonObject getV2RayStreamSettingsConfig(const QJsonObject& serverConfig);
+    QJsonArray getRandomUserAgents(int n);
+    QJsonObject getPrettyShadowsocksConfig(const QJsonObject& serverConfig);
 };
 
 #endif // APPPROXY_H
